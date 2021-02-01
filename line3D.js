ae8ac52d4d35c8c23670d9c2703f14aa21c01abb
Andy Velazquez <andyvelazquez13@yahoo.com>
2021-02-01 15:30:53 -0800

    3d line is not working right now. The series y titles are messing something up

--- line3D.js
+++ line3D.js
@@ -11,7 +11,7 @@ var counter = 0;
 //   "y series 2 data": [200, 300, 400, 550],
 //   "y series 3 data": [300, 400, 500, 750]
 // }
-
+//
 // param = JSON.stringify(param);
 
 
@@ -21,12 +21,14 @@ function main(param) {
   var chartTitle = fmJson["chart title"];
   var xData = fmJson["x data"]; //aray
   var yTitle = fmJson["y Title"];
-  // var ySeries1Title = fmJson["y series 1 title"];
-  // var ySeries2Title = fmJson["y series 2 title"];
-  // var ySeries3Title = fmJson["y series 3 title"];
+  var ySeries1Title = fmJson["y series 1 title"];
+  var ySeries2Title = fmJson["y series 2 title"];
+  var ySeries3Title = fmJson["y series 3 title"];
   var ySeries1Data = fmJson["y series 1 data"]; // array
   var ySeries2Data = fmJson["y series 2 data"]; // array
   var ySeries3Data = fmJson["y series 3 data"]; //array
+
+  var zAngle = fmJson["z angle"];
 
   var series1ForChart = [];
   var series2ForChart = [];
@@ -59,23 +61,28 @@ function main(param) {
   // chart.labels().format("{%x}: {%yPercentOfTotal}%");
   chart.title(chartTitle);
   chart.container("container");
-  chart.zAngle(20);
+  chart.zAngle(zAngle.parseInt(zAngle, 10));
   chart.zAspect("150%");
+  chart.legend(true);
   if(counter === 0){
-    chart.line(series1ForChart);
-    chart.line(series2ForChart);
-    chart.line(series3ForChart);
+    var series1 = chart.line(series1ForChart);
+    var series2 = chart.line(series2ForChart);
+    var series3 = chart.line(series3ForChart);
+    // series1.name(ySeries1Title);
+    // series2.name(ySeries2Title);
+    // series3.name(ySeries3Title);
     chart.draw();
     counter++;
   }
   else {
     chart.removeAllSeries();
-    chart.line(series1ForChart);
-    chart.line(series2ForChart);
-    chart.line(series3ForChart);
+    var series1 = chart.line(series1ForChart);
+    var series2 = chart.line(series2ForChart);
+    var series3 = chart.line(series3ForChart);
+    // series1.name(ySeries1Title);
+    // series2.name(ySeries2Title);
+    // series3.name(ySeries3Title);
     chart.draw();
-    counter = 0;
-
 
   }
 
